a41114b8500293ba949ccdde55e77dd50e3288e7
Lena <pjanvalena333@gmail.com>
2023-07-06 15:29:28 +0500

    Added navigation to all pages

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,22 +1,34 @@
 import "./App.css";
-import Catalog from "./Catalog"
-import Cart from "./Cart"
-import Order from "./Order"
-import PersonalAccount from "./PersonalAccount"
-import Product from "./Product"
+import Catalog from "./Catalog";
+import Cart from "./Cart";
+import Order from "./Order";
+import PersonalAccount from "./PersonalAccount";
+import Product from "./Product";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="catalog" element={<Catalog />} />
-        <Route path="cart" element={<Cart />} />
-        <Route path="order" element={<Order />} />
-        <Route path="personalAccount" element={<PersonalAccount />} />
-        <Route path="product/:id" element={<Product />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <a href="/cart">Корзина</a>
+          </li>
+          <li>
+            <a href="/catalog">Каталог</a>
+          </li>
+        </ul>
+      </nav>
+      <BrowserRouter>
+        <Routes>
+          <Route path="catalog" element={<Catalog />} />
+          <Route path="cart" element={<Cart />} />
+          <Route path="order" element={<Order />} />
+          <Route path="personalAccount" element={<PersonalAccount />} />
+          <Route path="product/:id" element={<Product />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
