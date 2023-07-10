17ad89607a176f8186ea9c7277aeba4442c2f51b
NikiforovRoman <romannikiforov86@gmail.com>
2023-07-10 19:52:29 +0500

    Добавлена валидация формы

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,7 +1,57 @@
 import "./Login.css";
 import Exit from "./Exit.svg";
+import {useState, useEffect} from 'react';
 
 const Login = () => {
+  const [tel, setTel] = useState('');
+  const [password, setPassword] = useState('');
+  const [telDirty, setTelDirty] = useState(false);
+  const [passwordDirty, setPasswordDirty] = useState(false);
+  const [telError, setTelError] = useState('Телефон не может быть пустым');
+  const [passwordError, setPasswordError] = useState('Пароль не может быть пустым');
+  const [formValid, setFormValid] = useState(false);
+
+  useEffect(() => {
+    if(telError || passwordError) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [telError, passwordError])
+
+  const telHandler = (e) => {
+    setTel(e.target.value)
+    const re = /^[\d\+][\d\(\)\ -]{4,14}\d$/;
+    if (re.test(String(e.target.value).toLowerCase())) {
+      setTelError('Телефон введён некорректно');
+    } else {
+      setTelError("");
+    }
+  }
+
+  const passwordHandler = (e) => {
+    setPassword(e.target.value)
+    if(e.target.value.length < 4) {
+      setPasswordError('Пароль должен быть длиннее 4');
+      if(!e.target.value) {
+        setPasswordError('Пароль не может быть пустым');
+      }
+    } else {
+      setPasswordError('');
+    }
+  }
+
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case 'tel':
+        setTelDirty(true);
+        break
+      case 'password':
+        setPasswordDirty(true);
+        break
+    } 
+  }
+
   return (
     <div className="login">
       <form className="login__form" method="post">
@@ -16,7 +66,12 @@ const Login = () => {
           <label className="login__label" for="tel__input">
             Телефон:
           </label>
+          {(telDirty && telError) && <div style={{color: red}}>{telError}</div>}
           <input
+            onChange={e => telHandler(e)}
+            value={tel}
+            onBlur={e => blurHandler(e)}
+            name="tel"
             className="login__input"
             id="tel__input"
             type="tel"
@@ -27,14 +82,19 @@ const Login = () => {
           <label className="login__label" for="password__input">
             Пароль:
           </label>
+          {(passwordDirty && passwordError) && <div style={{color: red}}>{passwordError}</div>}
           <input
+            onChange={e => passwordHandler(e)}
+            value={password}
+            onBlur={e => blurHandler(e)}
+            name="password"
             className="login__input"
             id="password__input"
             type="password"
             placeholder="qwerty"
           />
         </div>
-        <button className="login__submit" type="submit">
+        <button disabled={!formValid} className="login__submit" type="submit">
           Продолжить
         </button>
       </form>
