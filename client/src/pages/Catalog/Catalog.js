e66f1b8c86811fc5fdaf7601245b8aa76e4445df
Lena <pjanvalena333@gmail.com>
2023-07-11 19:53:13 +0500

    Готова вёрстка в каталоге

--- client/src/pages/Catalog/Catalog.js
+++ client/src/pages/Catalog/Catalog.js
@@ -10,6 +10,7 @@ const Catalog = () => {
   const [products, setProducts] = useState([]);
   const [isModalOpen, setModalOpen] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState(null);
+  const [selectedProductAmount, setSelectedProductAmount] = useState(0);
 
   useEffect(() => {
     setProducts([
@@ -29,6 +30,22 @@ const Catalog = () => {
         image:
           "https://www.tokyo-city.ru/images/interesno/Pitctca_-_natcionalnoe_italyanskoe_blyudo.jpg",
       },
+      {
+        id: 3,
+        name: "DFFR",
+        structure: "4132er23rf",
+        price: 100,
+        image:
+          "https://www.tokyo-city.ru/images/interesno/Pitctca_-_natcionalnoe_italyanskoe_blyudo.jpg",
+      },
+      {
+        id: 4,
+        name: "Xtndf",
+        structure: "adfsadf",
+        price: 400,
+        image:
+          "https://www.tokyo-city.ru/images/interesno/Pitctca_-_natcionalnoe_italyanskoe_blyudo.jpg",
+      },
     ]);
   }, []);
 
@@ -41,11 +58,15 @@ const Catalog = () => {
     setModalOpen(false);
   };
 
+  // при клике Добавить в корзину, selectedProductAmount будет количеством заказанной пиццы
+
   return (
     <>
       <SecondBackground />
       <div className="catalog-container">
-        <h2>Меню</h2>
+        <h2 style={{ marginBottom: "2em", textTransform: "uppercase" }}>
+          Меню
+        </h2>
         <div className="catalog-pizzas">
           {products.map((pizza) => (
             <div
@@ -53,12 +74,12 @@ const Catalog = () => {
               style={{
                 display: "flex",
                 flexDirection: "column",
-                margin: "0 1em",
+                margin: "1em",
               }}
             >
               <img src={pizza.image} alt="Пицца" />
               <h3>{pizza.name}</h3>
-              <p>{pizza.structure}</p>
+              <p style={{ margin: "1em 0" }}>{pizza.structure}</p>
               <div style={{ display: "flex", justifyContent: "space-between" }}>
                 <h4>{pizza.price} р.</h4>
                 <button
@@ -87,15 +108,46 @@ const Catalog = () => {
         <Modal
           isOpen={isModalOpen}
           onClose={closeModalCard}
-          width="20em"
+          width="30em"
           stylePosition={{
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
           }}
         >
-          <h2>{selectedProduct?.name}</h2>
-          <p>fighjk</p>
+          <h2 style={{ marginBottom: "1em" }}>{selectedProduct.name}</h2>
+          <img
+            src="https://www.tokyo-city.ru/images/interesno/Pitctca_-_natcionalnoe_italyanskoe_blyudo.jpg"
+            alt=""
+            width="100%"
+          />
+          <p style={{ margin: "1em 0" }}>{selectedProduct.structure}</p>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+          >
+            <button className="catalog-button">Добавить в корзину за {selectedProduct.price * selectedProductAmount} рублей</button>
+            <div>
+              <button
+                //onClick={() => decreaseAmount(product)}
+                className="cart-button-amount"
+              >
+                -
+              </button>
+              <span className="cart-product-amount">
+                {selectedProductAmount}
+              </span>
+              <button
+                //onClick={() => increaseAmount(product)}
+                className="cart-button-amount"
+              >
+                +
+              </button>
+            </div>
+          </div>
         </Modal>
       )}
     </>
