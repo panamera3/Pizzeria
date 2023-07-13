b5665683ac7e194acdc8a9890fa1b0a2bb430d3d
MrLamich <83765884+MrLamich@users.noreply.github.com>
2023-07-13 23:01:50 +0500

    Update Order.js

--- client/src/pages/Order/Order.js
+++ client/src/pages/Order/Order.js
@@ -1,6 +1,6 @@
 // libraries
 import { useLocalStorage } from "react-use";
-import { useEffect } from "react";
+import { useEffect, useState} from "react";
 import SecondBackground from "../../components/SecondBackground";
 // styles
 import "./Order.css";
@@ -8,7 +8,42 @@ import "./Order.css";
 const Order = () => {
   // формат: {productsPrice - сумма заказа, deliveryPrice -цена доставки, totalPrice - итоговая цена}
   const [orderInfo, setOrderInfo] = useLocalStorage("orderInfo");
+  const [cartProducts, setCartProducts] = useLocalStorage("cartProducts");
+  const [products, setProducts] = useState([{}]);
+  const [productsAmount, setProductsAmount] = useState(0);
+  const [productsPrice, setProductsPrice] = useState(0);
+  const [discountPrice, setDiscountPrice] = useState(0);
+  const [deliveryPrice, setDeliveryPrice] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
+  useEffect(() => {
+    setProducts(cartProducts);
+  }, []);
 
+  useEffect(() => {
+    setProductsAmount(
+      products.reduce((total, product) => total + product.amount, 0)
+    );
+    setProductsPrice(
+      products.reduce(
+        (total, product) => total + product.cost * product.amount,
+        0
+      )
+    );
+  }, [products]);
+
+  useEffect(() => {
+    setProducts(cartProducts);
+  }, [cartProducts]);
+
+  useEffect(() => {
+    setDeliveryPrice(
+      products.length > 0 ? (productsPrice >= 500 ? 0 : 200) : 0
+    );
+  }, [productsPrice]);
+
+  useEffect(() => {
+    setTotalPrice(productsPrice + deliveryPrice - discountPrice);
+  }, [productsPrice, deliveryPrice, discountPrice]);
   // в бд в метод "" передавать количество товаров в заказе, потом используется в ЛК это
 
   return (
@@ -35,6 +70,7 @@ const Order = () => {
         </div>
 
         <h3>АДРЕС И ВРЕМЯ ДОСТАВКИ</h3>
+        <div className="adressNCard">
         <div className="adressNTime">
           <div className="delivery">
             <button>Доставка</button>
@@ -44,6 +80,8 @@ const Order = () => {
             <button className="buttonNewAdress">Новый адрес</button>
             <button className="savedAdress">Сохранённый адрес</button>
           </div>
+          
+                
           <div className="adress">
             <p>Адрес*:</p>
             <input placeholder="Адрес" required />
@@ -65,6 +103,7 @@ const Order = () => {
               <p>Домофон:</p>
               <input placeholder="Есть" />
             </div>
+            
           </div>
 
           <div className="time">
@@ -93,7 +132,41 @@ const Order = () => {
             <textarea placeholder="Пожелания по доставке"></textarea>
           </div>
           <p className="bold">* поля должны быть обязательно заполнены</p>
-        </div>
+          </div>
+          <div className="card" style={{ position: "absolute", overflow: "visible" }}>
+                  <a
+                    href="https://yandex.ru/maps/54/yekaterinburg/?utm_medium=mapframe&utm_source=maps"
+                    style={{
+                      color: "#eee",
+                      fontSize: 12,
+                      position: "absolute",
+                      top: 0,
+                    }}
+                  >
+                    Екатеринбург
+                  </a>
+                  <a
+                    href="https://yandex.ru/maps/54/yekaterinburg/?ll=60.597465%2C56.838011&utm_medium=mapframe&utm_source=maps&z=12"
+                    style={{
+                      color: "#eee",
+                      fontSize: 12,
+                      position: "absolute",
+                      top: 14,
+                    }}
+                  >
+                    Яндекс Карты — транспорт, навигация, поиск мест
+                  </a>
+                  <iframe
+                    src="https://yandex.ru/map-widget/v1/?ll=60.597465%2C56.838011&z=12"
+                    width="612"
+                    height="400"
+                    frameBorder="1"
+                    allowFullScreen="true"
+                    style={{ position: "absolute" }}
+                  ></iframe>
+            </div>
+        
+      </div>
 
         <h3>СПОСОБ ОПЛАТЫ</h3>
         <div className="payment">
@@ -111,13 +184,13 @@ const Order = () => {
             <p>Доставка</p>
           </div>
           <div>
-            <p>2997 Р</p>
-            <p>0 Р</p>
+            <p>{productsPrice} Р</p>
+            <p>{deliveryPrice} Р</p>
           </div>
         </div>
         <div className="itog">
           <text>ИТОГО:</text>
-          <text>2997 Р</text>
+          <text>{totalPrice} Р</text>
           <button>Заказать</button>
         </div>
       </main>
